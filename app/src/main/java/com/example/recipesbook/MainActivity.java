7640dc0e06edfc56f4e7fdac0d90d083fcd040e1
Shady Mohammed <sa187@smail.ucas.edu.ps>
2026-01-21 18:06:21 +0200

    Set up Android project structure and base configuration

--- app/src/main/java/com/example/recipesbook/MainActivity.java
+++ app/src/main/java/com/example/recipesbook/MainActivity.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.recipesbook.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
     }
 }

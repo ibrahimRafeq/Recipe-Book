48b7d9d64ca9a57acbcfd10d9ca726f910a419d9
Abu Firas <ia25@smail.ucas.edu.ps>
2026-02-06 10:39:12 +0200

    Some improvements

--- app/src/main/java/com/example/recipesbook/ui_screens/ProfiloActivity.java
+++ app/src/main/java/com/example/recipesbook/ui_screens/ProfiloActivity.java
@@ -1,8 +1,12 @@
 package com.example.recipesbook.ui_screens;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.recipesbook.databinding.ActivityProfiloBinding;
 import com.google.firebase.auth.FirebaseAuth;
@@ -43,7 +47,6 @@ public class ProfiloActivity extends AppCompatActivity {
                             Picasso.get()
                                     .load(imageUrl)
                                     .into(binding.imgProfile3);
-
                         }
 
                     }
@@ -51,6 +54,24 @@ public class ProfiloActivity extends AppCompatActivity {
                 .addOnFailureListener(e -> {
                     Toast.makeText(this, "Data loading failed", Toast.LENGTH_SHORT).show();
                 });
+
+        binding.btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(ProfiloActivity.this)
+                        .setTitle("Logout")
+                        .setMessage("Are you sure you want to log out?")
+                        .setPositiveButton("Yes", (dialog, which) -> {
+                            FirebaseAuth.getInstance().signOut();
+                            startActivity(new Intent(ProfiloActivity.this, LoginScreen.class));
+                            finish();
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+
+            }
+        });
+
 
     }
 }

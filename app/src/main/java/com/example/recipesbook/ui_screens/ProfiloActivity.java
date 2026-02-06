c5a2e490678fc0d5199283a5a7ea29cd071cf839
Abu Firas <ia25@smail.ucas.edu.ps>
2026-02-06 14:30:25 +0200

    The password change section has been completed

--- app/src/main/java/com/example/recipesbook/ui_screens/ProfiloActivity.java
+++ app/src/main/java/com/example/recipesbook/ui_screens/ProfiloActivity.java
@@ -8,7 +8,10 @@ import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.recipesbook.databinding.ActivityProfiloBinding;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
 
@@ -78,5 +81,52 @@ public class ProfiloActivity extends AppCompatActivity {
                 binding.settingsDrawer.openDrawer(Gravity.LEFT);
             }
         });
+
+        binding.btnResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
+
+    private void changePassword(String oldPassword, String newPassword) {
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user == null) {
+            Toast.makeText(this, "لا يوجد مستخدم مسجل دخول", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String email = user.getEmail();
+
+        AuthCredential credential =
+                EmailAuthProvider.getCredential(email, oldPassword);
+
+        // إعادة التوثيق
+        user.reauthenticate(credential).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+
+                // تغيير كلمة السر
+                user.updatePassword(newPassword)
+                        .addOnCompleteListener(task2 -> {
+                            if (task2.isSuccessful()) {
+                                Toast.makeText(this,
+                                        "تم تغيير كلمة السر بنجاح ✅",
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(this,
+                                        "فشل تغيير كلمة السر ❌",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+            } else {
+                Toast.makeText(this,
+                        "كلمة السر القديمة غير صحيحة ❌",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
